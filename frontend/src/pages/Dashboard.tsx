12b675aef023e7b856f12e49199beed2b831df05
Jose Delgado <j3leonardodelgado@gmail.com>
2024-11-02 04:00:35 -0400

    updated: refactor Dashboard component to use Material Table instead of DataGrid, improving layout and styling for reservations display

--- frontend/src/pages/Dashboard.tsx
+++ frontend/src/pages/Dashboard.tsx
@@ -1,40 +1,21 @@
 import React, { useEffect, useState } from 'react';
-import { DataGrid, GridColDef } from '@mui/x-data-grid';
-import Paper from '@mui/material/Paper';
 
 import {
     getAllReservationsService,
     ReservationData,
 } from '../services/reservationService';
+import {
+    Container,
+    Table,
+    TableBody,
+    TableCell,
+    TableContainer,
+    TableHead,
+    TableRow,
+} from '@mui/material';
 
-const columns: GridColDef[] = [
-    {
-        field: 'fullName',
-        headerName: 'Full name',
-        description: 'This column has a value getter and is not sortable.',
-        sortable: false,
-        width: 150,
-        valueGetter: (value, row) =>
-            `${row.firstName || ''} ${row.lastName || ''}`,
-    },
-    { field: 'documentType', headerName: 'Document Type', width: 150 },
-    { field: 'documentNumber', headerName: 'Document Number', width: 150 },
-    { field: 'reservationDate', headerName: 'Reservation date', width: 200 },
-    { field: 'reservationType', headerName: 'Reservation type', width: 200 },
-    { field: 'numberOfPeople', headerName: 'Number of people', width: 100 },
-    { field: 'status', headerName: 'Status', width: 100 },
-    { field: 'email', headerName: 'Email', width: 200 },
-    {
-        field: 'description',
-        headerName: 'Description',
-        description: 'This column has a value getter and is not sortable.',
-        sortable: false,
-        width: 200,
-        valueGetter: (value, row) => `${row.description || ''} `,
-    },
-];
+import dayjs from 'dayjs';
 
-const paginationModel = { page: 0, pageSize: 5 };
 export const Dashboard: React.FC = () => {
     const [reservations, setReservations] = useState<ReservationData[] | null>(
         []
@@ -47,18 +28,48 @@ export const Dashboard: React.FC = () => {
         fetchReservations();
     }, []);
     return (
-        <Paper sx={{ height: 400, width: '100%', margin: 'auto', padding: 1 }}>
-            <h1 style={{ textAlign: 'center' }}>Dashboard</h1>
-            {reservations && (
-                <DataGrid
-                    rows={reservations}
-                    columns={columns}
-                    initialState={{ pagination: { paginationModel } }}
-                    pageSizeOptions={[5, 10]}
-                    checkboxSelection
-                    sx={{ border: 2 }}
-                />
-            )}
-        </Paper>
+        <TableContainer
+            component={Container}
+            sx={{ borderRadius: 5, backgroundColor: 'grey' }}
+        >
+            <Table aria-label='reservations table'>
+                <TableHead>
+                    <TableRow>
+                        <TableCell>Full name</TableCell>
+                        <TableCell>Document number</TableCell>
+                        <TableCell>Date of reservation</TableCell>
+                        <TableCell>Number of people</TableCell>
+                        <TableCell>Status</TableCell>
+                    </TableRow>
+                </TableHead>
+                <TableBody>
+                    {reservations?.map((reservation) => (
+                        <TableRow
+                            sx={{
+                                ':hover': {
+                                    backgroundColor: 'rgba(0, 0, 0, 0.04)',
+                                },
+                            }}
+                            key={reservation.id}
+                            onClick={() => {
+                                console.log(reservation.id);
+                            }}
+                        >
+                            <TableCell component='th' scope='row'>
+                                {reservation.firstName} {reservation.lastName}
+                            </TableCell>
+                            <TableCell>{reservation.documentNumber}</TableCell>
+                            <TableCell>
+                                {dayjs(reservation.reservationDate).format(
+                                    'MM/DD/YYYY'
+                                )}
+                            </TableCell>
+                            <TableCell>{reservation.numberOfPeople}</TableCell>
+                            <TableCell>{reservation.status}</TableCell>
+                        </TableRow>
+                    ))}
+                </TableBody>
+            </Table>
+        </TableContainer>
     );
 };
