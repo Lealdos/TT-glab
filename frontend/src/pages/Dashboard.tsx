8cccd8021f0f73fc01638aaf8eccd14c4994ab42
Jose Delgado <j3leonardodelgado@gmail.com>
2024-11-04 15:22:38 -0500

    updated: enhanced Dashboard page with row click functionality and modal for reservation details using MUI components in Dashboard.tsx

--- frontend/src/pages/Dashboard.tsx
+++ frontend/src/pages/Dashboard.tsx
@@ -15,11 +15,25 @@ import {
 } from '@mui/material';
 
 import dayjs from 'dayjs';
+import { ReservationModal } from '../components/Modal';
 
 export const Dashboard: React.FC = () => {
     const [reservations, setReservations] = useState<ReservationData[] | null>(
         []
     );
+    const [openModal, setOpenModal] = useState(false);
+    const [selectedReservation, setSelectedReservation] =
+        useState<ReservationData | null>(null);
+
+    const handleRowClick = (reservation: ReservationData) => {
+        setSelectedReservation(reservation);
+        setOpenModal(true);
+    };
+
+    const handleCloseModal = () => {
+        setSelectedReservation(null);
+        setOpenModal(false);
+    };
     useEffect(() => {
         const fetchReservations = async () => {
             const ReservationData = await getAllReservationsService();
@@ -28,60 +42,74 @@ export const Dashboard: React.FC = () => {
         fetchReservations();
     }, []);
     return (
-        <TableContainer
-            component={Container}
-            sx={{ borderRadius: 5, backgroundColor: 'grey' }}
+        <Container
+            sx={{
+                display: 'flex',
+                flexDirection: 'column',
+                alignItems: 'center',
+                backgroundColor: 'grey',
+                borderRadius: 5,
+                boxShadow: '0 0 10px rgba(0, 0, 0, 0.5)',
+            }}
         >
-            <Table aria-label='reservations table'>
-                <TableHead>
-                    <TableRow>
-                        <TableCell>Full name</TableCell>
-                        <TableCell>Document Type</TableCell>
-                        <TableCell>Document number</TableCell>
-                        <TableCell align='center'>Email</TableCell>
-                        <TableCell>Date of reservation</TableCell>
-                        <TableCell>Number of people</TableCell>
-                        <TableCell>Status</TableCell>
-                    </TableRow>
-                </TableHead>
-                <TableBody>
-                    {reservations?.map((reservation) => (
-                        <TableRow
-                            sx={{
-                                ':hover': {
-                                    backgroundColor: 'rgba(0, 0, 0, 0.08)',
-                                },
-                            }}
-                            key={reservation.id}
-                            onClick={() => {
-                                console.log(reservation.id);
-                            }}
-                        >
-                            <TableCell component='th' scope='row'>
-                                {reservation.firstName} {reservation.lastName}
-                            </TableCell>
-                            <TableCell align='center'>
-                                {reservation.documentType}
-                            </TableCell>
-                            <TableCell align='center'>
-                                {reservation.documentNumber}
-                            </TableCell>
-                            <TableCell align='center'>
-                                {reservation.email}
-                            </TableCell>
-                            <TableCell>
-                                {dayjs(reservation.reservationDate).format(
-                                    'MM/DD/YYYY hh:mm A'
-                                )}
-                            </TableCell>
-                            <TableCell align='center'>
-                                {reservation.numberOfPeople}
-                            </TableCell>
-                            <TableCell>{reservation.status}</TableCell>
+            <TableContainer component={Container} sx={{ borderRadius: 5 }}>
+                <Table aria-label='reservations table'>
+                    <TableHead>
+                        <TableRow>
+                            <TableCell>Full name</TableCell>
+                            <TableCell>Document Type</TableCell>
+                            <TableCell>Document number</TableCell>
+                            <TableCell align='center'>Email</TableCell>
+                            <TableCell>Date of reservation</TableCell>
+                            <TableCell>Number of people</TableCell>
+                            <TableCell>Status</TableCell>
                         </TableRow>
-                    ))}
-                </TableBody>
-            </Table>
-        </TableContainer>
+                    </TableHead>
+                    <TableBody>
+                        {reservations?.map((reservation) => (
+                            <TableRow
+                                sx={{
+                                    ':hover': {
+                                        backgroundColor: 'rgba(0, 0, 0, 0.08)',
+                                    },
+                                    cursor: 'pointer',
+                                    color: 'white',
+                                }}
+                                key={reservation.id}
+                                onClick={() => handleRowClick(reservation)}
+                            >
+                                <TableCell component='th' scope='row'>
+                                    {reservation.firstName}{' '}
+                                    {reservation.lastName}
+                                </TableCell>
+                                <TableCell align='center'>
+                                    {reservation.documentType}
+                                </TableCell>
+                                <TableCell align='center'>
+                                    {reservation.documentNumber}
+                                </TableCell>
+                                <TableCell align='center'>
+                                    {reservation.email}
+                                </TableCell>
+                                <TableCell>
+                                    {dayjs(reservation.reservationDate).format(
+                                        'MM/DD/YYYY hh:mm A'
+                                    )}
+                                </TableCell>
+                                <TableCell align='center'>
+                                    {reservation.numberOfPeople}
+                                </TableCell>
+                                <TableCell>{reservation.status}</TableCell>
+                            </TableRow>
+                        ))}
+                    </TableBody>
+                </Table>
+            </TableContainer>
+            <ReservationModal
+                open={openModal}
+                onClose={handleCloseModal}
+                reservation={selectedReservation}
+            />
+        </Container>
     );
 };
