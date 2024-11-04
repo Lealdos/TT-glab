822595167daf240031453d2ba0bf7308fcacd5c2
Jose Delgado <j3leonardodelgado@gmail.com>
2024-11-04 14:01:04 -0500

    updated: refactored Layout component to use MUI Container for improved styling and layout management in Layout.tsx

--- frontend/src/components/Layout.tsx
+++ frontend/src/components/Layout.tsx
@@ -1,19 +1,10 @@
+import { Container } from '@mui/material';
 import { Outlet } from 'react-router-dom';
 
 export const Layout: React.FC = () => {
     return (
-        <div
-            style={{
-                backgroundColor: '#242424',
-                height: '100vh',
-                display: 'flex',
-                flexDirection: 'column',
-                alignItems: 'center',
-                justifyContent: 'center',
-                overflow: 'auto',
-            }}
-        >
+        <Container>
             <Outlet />
-        </div>
+        </Container>
     );
 };
