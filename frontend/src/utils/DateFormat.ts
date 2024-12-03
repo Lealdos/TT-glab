c2bfa0765f026214007bbe1057b02d604ce70d33
Jose Delgado <j3leonardodelgado@gmail.com>
2024-12-03 01:10:22 -0500

    fix: update formatDateTime function to standardize date formatting to 'YYYY-MM-DDTHH:mm' for better consistency

--- frontend/src/utils/DateFormat.ts
+++ frontend/src/utils/DateFormat.ts
@@ -1,4 +1,4 @@
 import dayjs, { Dayjs } from 'dayjs';
 export function formatDateTime(date: Dayjs | Date | string): string {
-    return dayjs(date).format('MM-DD-YYYY hh:mm A');
+    return dayjs(date).format('YYYY-MM-DDTHH:mm');
 }
