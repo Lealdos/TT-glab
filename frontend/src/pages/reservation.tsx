48850bbd4c9efefa8a40af982b9fae3a2a14c1a8
Jose Delgado <j3leonardodelgado@gmail.com>
2025-01-02 03:29:57 -0500

    feat: add reservation form validation with zod and integrate reservation submission logic for improved user experience

--- frontend/src/pages/reservation.tsx
+++ frontend/src/pages/reservation.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import {
     TextField,
     Button,
@@ -10,15 +10,83 @@ import {
     Container,
 } from '@mui/material';
 import { MobileDateTimePicker } from '@mui/x-date-pickers/MobileDateTimePicker';
-import { Controller } from 'react-hook-form';
+import { Controller, SubmitHandler, useForm } from 'react-hook-form';
 
-import { useReservationViewModel } from '../viewModels/useReservationViewModel';
 import dayjs, { Dayjs } from 'dayjs';
 import { Layout } from '../components/Layout';
+import {
+    ReservationTypeSchema,
+    DocumentTypeSchema,
+} from '../utils/ValidationSchema';
+import { z } from 'zod';
+import { zodResolver } from '@hookform/resolvers/zod';
+import { useNavigate } from 'react-router-dom';
+import { createReservationService } from '../services/reservationService';
+import { formatDateTime } from '../utils/DateFormat';
+import { toast } from 'react-toastify';
+
+const DateSchema = z
+    .custom<Dayjs | null>(
+        (val) => dayjs(val).isValid(),
+
+        {
+            message: 'Valid date is required',
+        }
+    )
+    .refine((val) => dayjs(val).isAfter(dayjs().add(1, 'hour')), {
+        message: 'Date must be at least 1 hour from now',
+    });
+
+const reservationSchema = z.object({
+    reservationDate: DateSchema,
+    firstName: z.string().min(1, 'First name is required'),
+    lastName: z.string().min(1, 'Last name is required'),
+    documentType: DocumentTypeSchema,
+    documentNumber: z.string().min(5, 'Document number is required'),
+    email: z.string().email('Invalid email address'),
+    reservationType: ReservationTypeSchema,
+    numberOfPeople: z.number().min(1, 'At least one person is required'),
+    description: z.string().optional(),
+});
+
+type ReservationFormInputs = z.infer<typeof reservationSchema>;
 
 export const Reservation: React.FC = () => {
-    const { register, handleSubmit, errors, submitError, onSubmit, control } =
-        useReservationViewModel();
+    const {
+        register,
+        handleSubmit,
+        formState: { errors },
+        reset,
+        control,
+    } = useForm<ReservationFormInputs>({
+        resolver: zodResolver(reservationSchema),
+    });
+
+    const [submitError, setSubmitError] = useState<string | null>(null);
+    const navigate = useNavigate(); //redirigir hook
+
+    const onSubmit: SubmitHandler<ReservationFormInputs> = async ({
+        reservationDate,
+        ...restData
+    }) => {
+        try {
+            if (reservationDate) {
+                const formattedDate = formatDateTime(reservationDate); // Convert Dayjs to string
+                await createReservationService({
+                    ...restData,
+                    reservationDate: formattedDate,
+                    status: 'PENDING',
+                });
+                setSubmitError(null);
+                reset();
+                navigate('/thank-you');
+                toast('Reservation created successfully');
+            }
+        } catch (error: unknown) {
+            console.error(error);
+            setSubmitError('Failed to create reservation. Please try again.');
+        }
+    };
 
     return (
         <Layout>
